1e4ebcc16a1a45b81bdf54d8b51927b208431507
hasnainchandia77 <164181936+hasnainchandia77@users.noreply.github.com>
2024-08-04 11:41:13 +0500

    Add files via upload

---
+++ HASNAIN.py
@@ -0,0 +1,18 @@
+#খানকীর পোলা ফাইল পারলে চুরি করে দেখা 
+#তোর আব্বু তারেক এর ফাইল নিবি
+#I AM FUCK YOU
+import platform,os,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system("clear")
+    print("\x1b[38;5;154m YOUR DEVICE IS 64 BIT😍\x1b[1;97m")
+    time.sleep(3)
+    import FILE_RANDOM_64
+    MR_HASNAIN_64.loging()
+elif bit == '32bit':
+    os.system("clear")
+    print("\x1b[38;5;154m YOUR DEVICE IS 32 BIT 😍\x1b[1;97m")
+    time.sleep(3)
+    import FILE_RANDOM_32
+    MR_HASNAIN_32.loging()
+  
